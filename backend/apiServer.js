dc6e399cde4df6bc56e2afdde5934085c88bc1dc
LooKInWz4 <146947900+LooKInWzA@users.noreply.github.com>
2025-10-23 12:35:26 +0700

    Test

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -5,7 +5,7 @@ const multer = require('multer'); // Import multer
 const path = require('path'); // Import path for resolving upload directory
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 
